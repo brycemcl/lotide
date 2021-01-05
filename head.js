126d619d0e409d9132e5db2d30c19954fbb15655
Bryce McLachlan <brycemclachlan@gmail.com>
2021-01-05 17:02:34 +0000

    fixed lint warning ''head' is assigned a value but never used'

--- head.js
+++ head.js
@@ -1,6 +1,6 @@
 module.exports.head = (array) => array[0];
 
-const head = module.exports.head
+// const head = module.exports.head;
 // const { assertEqual } = require('./lotide');
 // assertEqual(head([5, 6, 7]), 5);
 // assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
