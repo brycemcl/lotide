3f0f0130cc659e62a925b769ef2ce0e5564d33d3
Bryce McLachlan <brycemclachlan@gmail.com>
2021-01-06 03:40:20 +0000

    Removed linting error out of eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,7 +1,7 @@
 const { isEqual } = require('./isEqual');
-const { assertEqual } = require('./assertEqual');
-module.exports.eqArrays = isEqual
+module.exports.eqArrays = isEqual;
 
+// const { assertEqual } = require('./assertEqual');
 // const eqArrays = isEqual
 // assertEqual((eqArrays([1, 2, 3], [1, 2, 3])), true)
 // assertEqual((eqArrays([1, 2, 3], [3, 2, 1])), false)
