b775294b315462667d6dd8dcb0da9b4756684ea2
Bryce McLachlan <brycemclachlan@gmail.com>
2021-01-06 03:53:35 +0000

    Implemented assertArraysEqual

--- lotide.js
+++ lotide.js
@@ -3,11 +3,13 @@ const { head } = require('./head');
 const { tail } = require('./tail');
 const { isEqual } = require('./isEqual');
 const { eqArrays } = require('./eqArrays');
+const { assertArraysEqual } = require('./assertArraysEqual');
 module.exports =
 {
   assertEqual,
   head,
   tail,
   isEqual,
-  eqArrays
+  eqArrays,
+  assertArraysEqual
 };
