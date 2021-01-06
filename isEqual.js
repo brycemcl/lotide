56635c62859878fb2c85cfea8bacdf680e0edb1a
Bryce McLachlan <brycemclachlan@gmail.com>
2021-01-06 03:39:25 +0000

    Implemented eqArrays and refactored isEqual out of assertEqual

---
+++ isEqual.js
@@ -0,0 +1,25 @@
+module.exports.isEqual = (actual, expected) => {
+
+  //https://stackoverflow.com/questions/5467129/sort-javascript-object-by-key
+  const sortObject = (object) => {
+    return Object.keys(object).sort().reduce(
+      (obj, key) => {
+        obj[key] = object[key];
+        return obj;
+      },
+      {}
+    );
+  };
+  if (typeof actual === "object") {
+    actual = JSON.stringify(sortObject(actual));
+  }
+  if (typeof expected === "object") {
+    expected = JSON.stringify(sortObject(expected));
+  }
+
+  if (actual === expected) {
+    return true;
+  } else {
+    return false;
+  }
+};
--- lotide.js
+++ lotide.js
@@ -1,9 +1,13 @@
 const { assertEqual } = require('./assertEqual');
 const { head } = require('./head');
 const { tail } = require('./tail');
+const { isEqual } = require('./isEqual');
+const { eqArrays } = require('./eqArrays');
 module.exports =
 {
   assertEqual,
   head,
-  tail
+  tail,
+  isEqual,
+  eqArrays
 };
