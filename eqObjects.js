a9fa0abdd48b12191f633b971925b9df6cd45adf
Bryce McLachlan <brycemclachlan@gmail.com>
2021-01-09 05:17:13 +0000

    added test cases for eqObject

--- eqObjects.js
+++ eqObjects.js
@@ -9,3 +9,8 @@ module.exports.eqObjects = isEqual;
 // assertEqual(eqObjects(ab, ba), true);
 // const abc = { a: "1", b: "2", c: "3" };
 // assertEqual(eqObjects(abc), false);
+
+////nested object test cases
+// console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); //true
+// console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); //false
+// console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }));  //false
--- map.js
+++ map.js
@@ -7,15 +7,15 @@ module.exports.map = (arrayArg, functionArg) => {
 };
 
 
-// const { assertEqual } = require('./assertEqual');
-// const map = module.exports.map;
-// const words = ["ground", "control", "to", "major", "tom"];
-// assertEqual(map(words, i => i + i), ['groundground', 'controlcontrol', 'toto', 'majormajor', 'tomtom']);
-// assertEqual(map(words, i => i + " here we go"), [
-//   'ground here we go',
-//   'control here we go',
-//   'to here we go',
-//   'major here we go',
-//   'tom here we go'
-// ]);
-// assertEqual(map(words, i => i === "ground"), [true, false, false, false, false]);
+const { assertEqual } = require('./assertEqual');
+const map = module.exports.map;
+const words = ["ground", "control", "to", "major", "tom"];
+assertEqual(map(words, i => i + i), ['groundground', 'controlcontrol', 'toto', 'majormajor', 'tomtom']);
+assertEqual(map(words, i => i + " here we go"), [
+  'ground here we go',
+  'control here we go',
+  'to here we go',
+  'major here we go',
+  'tom here we go'
+]);
+assertEqual(map(words, i => i === "ground"), [true, false, false, false, false]);
