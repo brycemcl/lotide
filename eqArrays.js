2a985a3e34b79e555a0f8740076205eac8d2581d
Bryce McLachlan <brycemclachlan@gmail.com>
2021-01-09 05:21:36 +0000

    added test cases for eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -2,8 +2,12 @@ const { isEqual } = require('./isEqual');
 module.exports.eqArrays = isEqual;
 
 // const { assertEqual } = require('./assertEqual');
-// const eqArrays = isEqual
+// const eqArrays = isEqual;
 // assertEqual((eqArrays([1, 2, 3], [1, 2, 3])), true)
 // assertEqual((eqArrays([1, 2, 3], [3, 2, 1])), false)
 // assertEqual((eqArrays(["1", "2", "3"], ["1", "2", "3"])), true)
 // assertEqual((eqArrays(["1", "2", "3"], ["1", "2", 3])), false)
+// //test cases for nested arrays
+// console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+// console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
+// console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
